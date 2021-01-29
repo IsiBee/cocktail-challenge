7ad120bce953369643dd92b3a2c6e7a731a8de1d
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-01-28 21:07:01 -0600

    added more branding and the display drink API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,24 @@ var ingredientFormEl = document.querySelector("#ingredient-form");
 var ingredientInputEl = document.querySelector("#entered-ingredient");
 var drinkListEl = document.querySelector("#list-of-drinks");
 
-let DRINKS = []
 // get list of drinks from user entered ingredient
 
 function getDrinkList(ingredient) {
     var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
 
-    $.get(apiUrl)
-        .then(displayDrinkList)
-        .catch(function (err) {
-            console.warn(err)
-        })
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayDrinkList(data);
+            });
+        }
+        else {
+            console.log("Error");
+        }
+    })
+        .catch(
+            console.log("Error")
+        );
 
 };
 
@@ -25,26 +32,26 @@ function displayDrinkList(drinkList) {
         // create list element to hold the drink name
         var drinkEl = document.createElement("div");
         drinkEl.classList.add("panel-block");
-        drinkEl.setAttribute("data-drink-id", DRINKS[i].idDrink)
-        drinkEl.addEventListener("click", displayDrink)
+        drinkEl.setAttribute("data-drink-id", DRINKS[i].idDrink);
+        drinkEl.addEventListener("click", displayDrink);
         drinkEl.textContent = DRINKS[i].strDrink;
         drinkListEl.appendChild(drinkEl);
     }
 };
 
 function displayDrink() {
-    var id = this.getAttribute("data-drink-id")
-    var drink;
+    var id = this.getAttribute("data-drink-id");
 
-    for (var i = 0; i < DRINKS.length; i++) {
-        if (DRINKS[i].idDrink === id) {
-            drink = DRINKS[i]
-            break;
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id;
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
         }
-    }
-
-    console.log(drink)
-}
+    });
+};
 
 
 // What to do on button click
