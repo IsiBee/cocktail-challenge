32024d9e9d7a1afec070e4ab930d1f209c4c1229
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-02-02 19:10:42 -0600

    fixed the drink image stretching

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,12 +74,14 @@ function displayDrinkCard(drink) {
     recipeEl.classList = "card-content columns";
 
     // Pulls the Image of the drink from the drink object
+    var imageDiv = document.createElement("div");
     var imageEl = document.createElement("img");
     imageEl.setAttribute("src", drink.strDrinkThumb);
-    imageEl.setAttribute("width", "400px");
-    imageEl.classList = "content card-image image column is-one-third is-hidden-mobile";
+    imageDiv.classList = "column one-third"
+    imageEl.classList = "content card-image image tile is-child is-hidden-mobile";
 
-    recipeEl.appendChild(imageEl);
+    imageDiv.appendChild(imageEl);
+    recipeEl.appendChild(imageDiv);
 
     // Pull the Ingredients from the drink object
     // Pull the Measurements from the drink object
